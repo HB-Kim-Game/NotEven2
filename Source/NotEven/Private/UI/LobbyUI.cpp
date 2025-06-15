a76119f2da2ff93ebf2a3209f0eda284eec2aeac
HB-Kim-Game <hbkim0909@gmail.com>
2025-06-16 08:56:58 +0900

    LobbyUI InputMode 수정

--- Source/NotEven/Private/UI/LobbyUI.cpp
+++ Source/NotEven/Private/UI/LobbyUI.cpp
@@ -10,10 +10,16 @@ void ULobbyUI::NativeConstruct()
 	Super::NativeConstruct();
 
 	StartButton->OnClicked.AddDynamic(this, &ULobbyUI::StartGame);
+
+	GetWorld()->GetFirstPlayerController()->SetInputMode(FInputModeUIOnly());
+	GetWorld()->GetFirstPlayerController()->SetShowMouseCursor(true);
 }
 
 void ULobbyUI::StartGame()
 {
+	GetWorld()->GetFirstPlayerController()->SetInputMode(FInputModeGameOnly());
+	GetWorld()->GetFirstPlayerController()->SetShowMouseCursor(false);
+	
 	if (GetWorld()->GetFirstPlayerController()->HasAuthority())
 	{
 		GetWorld()->ServerTravel("/Game/KHB/Maps/MapDesign?listen", true);
